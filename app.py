7b925b2fb8c6730fc2747720c3fec3a2e7bc3c65
erykzarebski <erykzarebski0@gmail.com>
2023-12-04 00:00:56 +0100

    progress

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ NEO4J_USERNAME = os.environ.get('NEO4J_USERNAME')
 NEO4J_PASSWORD = os.environ.get('NEO4J_PASSWORD')
 AURA_INSTANCEID = os.environ.get('AURA_INSTANCEID')
 AURA_INSTANCENAME = os.environ.get('AURA_INSTANCENAME')
-print(app.secret_key)
 
 class Neo4jDB:
     def __init__(self):
@@ -31,18 +30,28 @@ class Neo4jDB:
             result = session.run(query, parameters)
             return result.data()
 
-
 db = Neo4jDB()
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.django-html")
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
+@app.route("/profile/<string:username>")
+@app.route("/profile", defaults={'username': None}) 
+def profile(username):
+    current_user = User.get_user(db, session["username"])
+
+    is_my_profile = True if (current_user.username == username 
+                                or username is None) else False
+    username = username or session["username"]
+    is_friend = current_user.is_friend(db, username)
+
+    ctx = {'username': username 
+    , 'is_my_profile': is_my_profile
+    , 'is_friend': is_friend }
+    return render_template("profile.django-html", ctx=ctx)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -50,14 +59,17 @@ def register():
         username = request.form["username"]
         password = request.form["password"]
         password_hash = generate_password_hash(password, method="sha256")
+        user = User(username, password_hash)
 
-        query = "CREATE (u:User {username: $username, password_hash: $password_hash})"
-        db.execute_query(query, {"username": username, "password_hash": password_hash})
+        if not user.is_username_taken(db):
+            user.create_user(db)
 
-        flash("Registration successful. Please log in.")
-        return redirect(url_for("login"))
+            flash("Registration successful. Please log in.")
+            return redirect(url_for("login"))
+        else:
+            flash("Username already exists. ")
 
-    return render_template("register.html")
+    return render_template("register.django-html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -66,19 +78,18 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        query = "MATCH (u:User {username: $username}) RETURN u.password_hash AS password_hash"
-        result = db.execute_query(query, {"username": username})
-
-        if result:
-            user = User(username, result[0]["password_hash"])
+        user = User.get_user(db, username)
+        if user:
             if user.check_password(password):
                 session["username"] = username
                 flash("Login successful.")
                 return redirect(url_for("profile"))
             else:
                 flash("Invalid password. Please try again.")
+        else:
+            flash("Username does not exist.")
 
-    return render_template("login.html")
+    return render_template("login.django-html")
 
 @app.route("/logout")
 def logout():
@@ -86,6 +97,39 @@ def logout():
     flash("You have been logged out.")
     return redirect(url_for("home"))
 
+@app.route('/users')
+def users_list():
+    users = User.get_all_usernames(db)
+    return render_template('users_list.django-html', users=users)
+
+@app.route('/add_friend/<string:username>', methods=["POST"])
+def add_friend(username):
+    current_user = User.get_user(db, session["username"])
+    if session["username"] != username:
+        current_user.add_friendship(db, username)
+        flash(f'Added {username} to your friends!')
+    else:
+        flash("You can't add yourself as a friend.")
+    return redirect(url_for('profile', username=username))
+
+@app.route('/remove_friend/<string:username>', methods=["POST"])
+def remove_friend(username):
+    user = User.get_user(db, session["username"])
+
+    if user.is_friend(db, username):
+        user.remove_friendship(db, username)
+        flash(f'Removed {username} from your friends!')
+    else:
+        flash(f'{username} is not in your friends list.')
+
+    return redirect(url_for('profile', username=username))
+
+@app.route('/profile/<string:username>/friends')
+def friends(username):
+    user = User.get_user(db, username)
+    friends = user.get_friend_usernames(db)
+    ctx = {"friends": friends, "username": username }
+    return render_template('friends.django-html', ctx=ctx)
 
 if __name__ == '__main__':
    app.run(debug=True)
--- src/models/User.py
+++ src/models/User.py
@@ -5,5 +5,66 @@ class User:
         self.username = username
         self.password_hash = password_hash
 
+    @staticmethod
+    def get_user(db, username):
+        query = "MATCH (u:User {username: $username}) RETURN u LIMIT 1"
+        result = db.execute_query(query, {"username": username})
+        if result:
+            user = result[0]["u"]
+            return User(user["username"], user["password_hash"])
+        else:
+            return None
+
+    @staticmethod
+    def get_all_usernames(db):
+        query = "MATCH (u:User) RETURN u.username AS username"
+        result = db.execute_query(query)
+        return [record["username"] for record in result]
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def is_username_taken(self, db):
+        query = "MATCH (u:User {username: $username}) RETURN u.username AS username"
+        result = db.execute_query(query, {"username": self.username})
+        return bool(result)
+
+    def create_user(self, db):
+        query = "CREATE (u:User {username: $username, password_hash: $password_hash})"
+        params = {"username": self.username, "password_hash": self.password_hash}
+        return db.execute_query(query, params)
+
+    def is_friend(self, db, friend_username):
+        query = (
+            "MATCH (user:User {username: $current_user})-[:FRIENDS_WITH]-(friend:User {username: $friend_username}) "
+            "RETURN COUNT(friend) > 0 AS is_friend"
+        )
+        params = {"current_user": self.username, "friend_username": friend_username}
+        return db.execute_query(query, params)[0]["is_friend"]
+
+    def add_friendship(self, db, friend_username):
+        query = (
+            "MATCH (user:User {username: $current_user}), (friend:User {username: $friend_username}) "
+            "MERGE (user)-[:FRIENDS_WITH]->(friend)"
+            "MERGE (friend)-[:FRIENDS_WITH]->(user)"
+        )
+        params = {"current_user": self.username, "friend_username": friend_username}
+        return db.execute_query(query, params)
+        
+    def remove_friendship(self, db, friend_username):
+        query = (
+            "MATCH (user:User {username: $current_user})-[r:FRIENDS_WITH]-(friend:User {username: $friend_username}) "
+            "DELETE r"
+        )
+        params = {"current_user": self.username, "friend_username": friend_username}
+        return db.execute_query(query, params)
+
+    def get_friend_usernames(self, db):
+        query = (
+            "MATCH (user:User {username: $username})-[:FRIENDS_WITH]-(friend:User) "
+            "RETURN friend"
+        )
+        params = {"username": self.username}
+        result = db.execute_query(query, params)
+        return [record['friend']['username'] for record in result]
+
